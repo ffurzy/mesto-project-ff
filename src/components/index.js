5aff9f386d5e1d5d253e591dbab862bd8d7aa915
Dmitrii Musikhin <ffurzy@gmail.com>
2025-04-22 09:04:00 -0400

    working

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,38 @@
 import '../pages/index.css';
 import { initialCards } from './cards.js';
 import { createCard, deleteCard } from './card.js';
+import { openModal, closeModal, closeModalOverlay, closeModalEsc } from './modal.js';
 
 const cards = document.querySelector('.places__list');
+const AddCard = document.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_type_new-card');
+
+
+
+closeModalOverlay();
+//открываем попап при нажатии на +
+
+AddCard.addEventListener('click', function() {
+  openModal(popupAddCard);
+});
+
+//находим все попапы с крестиками и вешаем на них closeModal
+document.querySelectorAll('.popup__close').forEach(function(btn) {
+    const popup = btn.closest('.popup');
+    btn.addEventListener('click', function() {
+      closeModal(popup);
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 initialCards.forEach(function(item) {
   const newCard = createCard(item, deleteCard);
---
+++ src/components/modal.js
@@ -0,0 +1,34 @@
+//открываем модальное окно
+export function openModal(popup) {
+    popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closeModalEsc);
+};
+
+//закрываем модальное окно 
+export function closeModal(popup) {
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closeModalEsc);
+};
+
+//Закрываем модельно окно по оверлею
+export function closeModalOverlay(evt) {
+    const popups = document.querySelectorAll('.popup');
+  popups.forEach(function(popup) {
+    popup.addEventListener('mousedown', function(evt) {
+      if (evt.target === popup) {
+        closeModal(popup);
+      }
+    });
+  });
+}
+
+//Закрываем модельное окно по Esc
+export function closeModalEsc(evt) {
+    if (env.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened')
+    }
+    if (openedPopup) {
+        closeModal(openedPopup);
+    }
+};
+
